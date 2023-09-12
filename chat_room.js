c0821aa88a419c0a80115f40aa5cb74077c72ff8
mystery-mind <123958834+mystery-mind@users.noreply.github.com>
2023-09-12 20:18:48 +0530

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,26 @@
+
+firebase.initializeApp(firebaseConfig);
+ // Your web app's Firebase configuration
+
+var firebaseConfig = {
+
+  apiKey: "AIzaSyBe8NG5uYUE1FkPcfH58VWNhY1EACk0fhc",
+
+  authDomain: "classtest-9b50d.firebaseapp.com",
+
+  projectId: "classtest-9b50d",
+
+  storageBucket: "classtest-9b50d.appspot.com",
+
+  messagingSenderId: "704043017261",
+
+  appId: "1:704043017261:web:72cc7531a30368aaf9c4a4"
+
+};
+
+
+// Initialize Firebase
+
+firebase.initializeApp (firebaseConfig);
+
+
